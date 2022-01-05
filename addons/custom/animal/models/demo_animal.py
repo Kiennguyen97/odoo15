f4f99308c2d0b7791fe0839af3ad077002a5aea8
DevKinzz <devkinzz@outlook.com>
2022-01-05 08:33:47 +0700

    push

--- addons/custom/animal/models/demo_animal.py
+++ addons/custom/animal/models/demo_animal.py
@@ -26,23 +26,36 @@ class DemoAnimal(models.Model):
         res = super(DemoAnimal, self).write(vals)
         return res
 
+    # write lại data
     def button_update_to_dog(self):
         self.write({'name': 'DOG'})
 
+    # thay đổi state hiện tại thành block
     def change_block(self):
         self.state = 'block'
 
+    # Mỗi khi Name thay đổi thì state sẽ chuyển về confirm
     @api.constrains('name')
     def change_state(self):
         self.state = 'confirm'
 
+    # copy record hiện tại nhưng thay name = harrypotter
     def duplicate_animal(self):
         self.copy({'name': 'HarryPotter'})
 
+    # Tạo create default name sẽ là Naruto
     def default_get(self, fields_list):
         res = super(DemoAnimal, self).default_get(fields_list)
-        res['name'] = 'HarryPotter'
+        res['name'] = 'Naruto'
         return res
+
+    # Create new record voi name
+    def name_create(self, name):
+        res =  super(DemoAnimal, self).name_create(name)
+        return res
+
+    def create_new_ninja(self):
+        self.name_create('Sasuke')
 
 
 
