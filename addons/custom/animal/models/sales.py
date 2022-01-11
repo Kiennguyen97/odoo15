c31f005bb0a1e402edff940390a663890974f941
DevKinzz <devkinzz@outlook.com>
2022-01-11 16:20:05 +0700

    commit

--- addons/custom/animal/models/sales.py
+++ addons/custom/animal/models/sales.py
@@ -4,5 +4,4 @@ class AnimalSale(models.Model):
     _inherit = "sale.order"
 
     sale_description = fields.Char(string='Animal Sale Description')
-    date_order = fields.Char(string='Animal Order Date')
 
