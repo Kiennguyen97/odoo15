838e5dbca06c914d2742bed1c2d7407650aaa6d4
DevKinzz <devkinzz@outlook.com>
2022-01-25 14:30:17 +0700

    Chapter 14: Interact With Other Modules

--- addons/custom/estate_account/models/estate_property.py
+++ addons/custom/estate_account/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api, exceptions
+from odoo import models, fields, api, exceptions, Command
 
 """
 Inheritance property 
@@ -11,7 +11,24 @@ class EstateProperty(models.Model):
     def sold_property(self):
         res = super().sold_property()
         journal = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
-        vals = {'partner_id': self.id, 'move_type': 'Customer Invoice', 'journal_id': journal.id}
+        vals = {'partner_id': self.id, 'move_type': 'out_invoice', 'journal_id': journal.id,
+                    "invoice_line_ids": [
+                        (
+                            Command.create({
+                                "name": "Test",
+                                "quantity": "3",
+                                "price_unit": "100"
+                            })
+                        ),
+                        (
+                            Command.create({
+                                "name": "selling",
+                                "quantity": "1",
+                                "price_unit": float(self.selling_price*6/100)
+                            })
+                        )
+                    ]
+                }
         # vals = {'partner_id': self.buyer_id, 'move_type': 'Customer Invoice'}
         self.env['account.move'].create(vals)
         return res
