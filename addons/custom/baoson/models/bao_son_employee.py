4f352d7d1b84ee8c95037aaad06b21823ac1d9c1
KIENN-AHT <kiennv@arrowhitech.co>
2023-03-16 16:55:35 +0700

    test

--- addons/custom/baoson/models/bao_son_employee.py
+++ addons/custom/baoson/models/bao_son_employee.py
@@ -2,8 +2,9 @@ from odoo import models, fields
 
 
 class BaoSonEmployee(models.Model):
-    _name = "bao.son.employee"
     _inherit = "hr.employee"
+
+    # category_ids = fields.One2many('hr.employee.category', 'employee_id', string='Tags')
 
 
 
